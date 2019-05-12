df4b3b1369e53201180f8df8a790c1179f6d7ee4
Willson <tafengnanhai@foxmail.com>
2019-05-12 19:19:55 +0800

    突然运行异常，安装了babel-loader和glob的插件

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,7 +1,52 @@
 <template>
   <el-container>
     <transition name="bounce" enter-active-class="bounceInLeft" leave-active-class="bounceOutLeft">
-      <el-aside width="240px" v-show="showMenu">menu</el-aside>
+      <el-aside width="240px" v-show="showMenu">
+        <el-scrollbar>
+          <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+            <el-radio-button :label="false">展开</el-radio-button>
+            <el-radio-button :label="true">收起</el-radio-button>
+          </el-radio-group>
+          <el-menu
+            default-active="1-4-1"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            :collapse="isCollapse"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span slot="title">导航一</span>
+              </template>
+              <el-menu-item-group>
+                <span slot="title">分组一</span>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <span slot="title">选项4</span>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-scrollbar>
+      </el-aside>
     </transition>
 
     <el-container>
@@ -18,7 +63,8 @@
 export default {
   data () {
     return {
-      showMenu: true
+      showMenu: true,
+      isCollapse: true
     }
   },
   methods: {
@@ -32,6 +78,12 @@ export default {
         document.querySelector('.el-aside').style.left = '0px'
         this.showMenu = true
       }
+    },
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
     }
   },
   mounted () {
@@ -50,7 +102,17 @@ export default {
   background: white;
 }
 .el-aside {
-  background: red;
+  background: #2f353f;
+}
+.el-scrollbar {
+  height: 100%;
+}
+.is-horizontal {
+  display: none;
+}
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
 }
 .el-main {
   background: blue;
