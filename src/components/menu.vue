b466bddc8b203e915697a64360168ad7651e98fc
Willson <tafengnanhai@foxmail.com>
2019-05-13 16:52:49 +0800

    菜单独立成组件

---
+++ src/components/menu.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <el-menu
+      default-active="2"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#2f353f"
+      text-color="#909399"
+      active-text-color="white"
+    >
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group>
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-menu {
+  width: 100%;
+  border: 0;
+  display: block;
+  overflow: hidden;
+}
+.el-menu-item.is-active {
+  background-color: #272c35 !important;
+}
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,44 +2,7 @@
   <el-container>
     <el-aside width="240px" v-show="showMenu">
       <div class="site white f18 b white">TFView</div>
-      <el-menu
-        default-active="2"
-        @open="handleOpen"
-        @close="handleClose"
-        background-color="#2f353f"
-        text-color="#909399"
-        active-text-color="white"
-      >
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
-        </el-menu-item>
-      </el-menu>
+      <Menu/>
     </el-aside>
 
     <el-container>
@@ -56,22 +19,19 @@
   </el-container>
 </template>
 <script>
+import Menu from '@/components/menu.vue'
 export default {
   data () {
     return {
       showMenu: true,
-      isCollapse: true
     }
+  },
+  components: {
+    Menu
   },
   methods: {
     toggleMenu: function () {
       this.showMenu = !this.showMenu
-    },
-    handleOpen (key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleClose (key, keyPath) {
-      console.log(key, keyPath)
     }
   }
 }
