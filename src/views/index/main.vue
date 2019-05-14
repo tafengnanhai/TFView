24b227bc0dff832cf9a1ab78cc1bb9a0a3771d6e
Willson <tafengnanhai@foxmail.com>
2019-05-14 22:40:42 +0800

    首页面板

---
+++ src/views/index/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>index_main</div>
+</template>
+
+<script>
+export default {
+  name: 'index_main',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
