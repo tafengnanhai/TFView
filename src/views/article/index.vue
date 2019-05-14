4282fa8937b7cfb1dc76f556c61a8d300f1f35a2
Willson <tafengnanhai@foxmail.com>
2019-05-14 21:49:32 +0800

    文章管理

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <div class="panel">
+      <el-button type="primary" icon="el-icon-edit" size="medium">添加</el-button>&nbsp;&nbsp;
+      <el-input placeholder="请输入关键词" v-model="keyword" style="width:300px" size="medium"></el-input>&nbsp;&nbsp;
+      <el-button type="primary" icon="el-icon-search" size="medium">搜索</el-button>
+    </div>
     <el-table :data="tableData" border style="width: 100%">
       <el-table-column prop="date" label="日期" min-width="15%" align="center"></el-table-column>
       <el-table-column prop="name" label="姓名" min-width="10%" align="center"></el-table-column>
@@ -9,8 +14,8 @@
       <el-table-column prop="zip" label="邮编" min-width="10%" align="center"></el-table-column>
       <el-table-column label="操作" min-width="15%" align="center">
         <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-          <el-button type="text" size="small">编辑</el-button>
+          <el-button type="primary" size="small">编辑</el-button>
+          <el-button @click="handleClick(scope.row)" type="danger" size="small">删除</el-button>&nbsp;&nbsp;
         </template>
       </el-table-column>
     </el-table>
@@ -22,6 +27,7 @@ export default {
   name: 'article_index',
   data () {
     return {
+      keyword: '',
       tableData: [{
         date: '2016-05-02',
         name: '王小虎',
@@ -62,4 +68,9 @@ export default {
 </script>
 
 <style scoped>
+.panel {
+  width: 100%;
+  height: 40px;
+  padding-bottom: 15px;
+}
 </style>
