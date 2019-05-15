4b3dbc2935ffec8856f9d6483496e135bf1fa2b2
Willson <tafengnanhai@foxmail.com>
2019-05-15 21:01:36 +0800

    首页面板

--- src/views/index/main.vue
+++ src/views/index/main.vue
@@ -2,16 +2,68 @@
   <div>
     <el-row :gutter="20">
       <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-        <el-card class="statCard"></el-card>
+        <el-card class="statCard">
+          <el-row>
+            <el-col :span="8">
+              <div class="cardIcon" style="background: #fbc677">
+                <i class="el-icon-s-custom f26 white"/>
+              </div>
+            </el-col>
+            <el-col :span="8" :offset="8" class="cardNum">
+              <span class="f20 b">7001</span>
+              <br>
+              <br>今日注册
+            </el-col>
+          </el-row>
+        </el-card>
       </el-col>
       <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-        <el-card class="statCard"></el-card>
+        <el-card class="statCard">
+          <el-row>
+            <el-col :span="8">
+              <div class="cardIcon" style="background:#ea6967">
+                <i class="el-icon-s-custom f26 white"/>
+              </div>
+            </el-col>
+            <el-col :span="8" :offset="8" class="cardNum">
+              <span class="f20 b">70010</span>
+              <br>
+              <br>本周注册
+            </el-col>
+          </el-row>
+        </el-card>
       </el-col>
       <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-        <el-card class="statCard"></el-card>
+        <el-card class="statCard">
+          <el-row>
+            <el-col :span="8">
+              <div class="cardIcon" style="background:#966ae2">
+                <i class="el-icon-s-custom f26 white"/>
+              </div>
+            </el-col>
+            <el-col :span="8" :offset="8" class="cardNum">
+              <span class="f20 b">700100</span>
+              <br>
+              <br>本月注册
+            </el-col>
+          </el-row>
+        </el-card>
       </el-col>
       <el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6">
-        <el-card class="statCard"></el-card>
+        <el-card class="statCard">
+          <el-row>
+            <el-col :span="8">
+              <div class="cardIcon" style="background:#67a7fb">
+                <i class="el-icon-s-custom f26 white"/>
+              </div>
+            </el-col>
+            <el-col :span="8" :offset="8" class="cardNum">
+              <span class="f20 b">7001001</span>
+              <br>
+              <br>累计注册
+            </el-col>
+          </el-row>
+        </el-card>
       </el-col>
     </el-row>
     <el-row :gutter="20">
@@ -39,8 +91,16 @@ export default {
 <style scoped>
 .statCard {
   width: 100%;
-  height: 150px;
+  height: 100px;
   margin-bottom: 20px;
+  color: #797979;
+  text-align: right;
+}
+.cardIcon {
+  width: 26px;
+  height: 26px;
+  padding: 17px;
+  border-radius: 30px;
 }
 .chartCard {
   height: 500px;
