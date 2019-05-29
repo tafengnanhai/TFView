63c3ddbcd807a63a1f2462365205a44a7ab3f538
Willson <tafengnanhai@foxmail.com>
2019-05-29 10:42:26 +0800

    优化默认参数

--- src/plugins/message.js
+++ src/plugins/message.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-
-Vue.use(ElementUI)
 
 export default {
   success: function (message, duration = 1000) {
-    return Vue.prototype.$message({ message: message, type: 'success', duration: duration })
+    return Vue.prototype.$message({
+      message: message,
+      type: 'success',
+      duration: duration
+    })
   },
-  error: function (message, duration = 1000) {
-    return Vue.prototype.$message({ message: message, type: 'error', duration: duration })
+  error: function (message, duration = 1500) {
+    return Vue.prototype.$message({
+      message: message,
+      type: 'error',
+      duration: duration
+    })
   }
 }
