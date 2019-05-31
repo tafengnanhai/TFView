6bd15ca085ba1f8a567013b26c4f10362fb15683
Willson <tafengnanhai@foxmail.com>
2019-05-31 19:14:11 +0800

    修改密码

---
+++ src/mock/Admin.js
@@ -0,0 +1,101 @@
+import Mock from 'mockjs'
+import store from '@/store'
+import Tools from '@/plugins/tools'
+
+const pageSize = store.state.pageSize
+let maxId = 2
+
+const extraData = [
+  {
+    admin_id: 1,
+    admin_username: 'other',
+    admin_password: '123456'
+  },
+  {
+    admin_id: 2,
+    admin_username: 'admin',
+    admin_password: 'admin'
+  }
+]
+
+let dataListAll = {
+  code: 0,
+  msg: '操作成功',
+  pageSize: pageSize,
+  total: maxId
+}
+
+const dataLoginSuccess = {
+  code: 0,
+  msg: '登录成功',
+  extra: { token: store.state.testToken }
+}
+
+const dataLoginError = {
+  code: 1,
+  msg: '用户名或密码错误'
+}
+
+// 和后端一致，不建议直接返回用户名和密码的判断方式
+Mock.mock(/\/Admin\/check/, 'post', function (options) {
+  const result = JSON.parse(options.body)
+  if (
+    extraData.some(
+      item =>
+        item.admin_username === result.username &&
+        item.admin_password === result.password
+    )
+  ) {
+    return dataLoginSuccess
+  }
+  return dataLoginError
+})
+
+const dataEditSuccess = {
+  code: 0,
+  msg: '操作成功'
+}
+
+const dataEditError = {
+  code: 1,
+  msg: '用户名不存在或者已删除'
+}
+Mock.mock(/\/Article\/add/, 'post', function (options) {
+  const result = JSON.parse(options.body)
+  result.admin_id = ++maxId
+  extraData.extra.push(result)
+  extraData.total++
+  return dataEditSuccess
+})
+
+Mock.mock(/\/Article\/edit/, 'post', function (options) {
+  const result = JSON.parse(options.body)
+  let isExists = false
+  extraData.extra = extraData.extra.map(item => {
+    if (item.admin_username === result.admin_username) {
+      isExists = true
+      Object.assign(item, result)
+    }
+    return item
+  })
+  if (!isExists) {
+    return dataEditError
+  }
+  return dataEditSuccess
+})
+
+Mock.mock(/\/Article\/detail/, 'get', function (options) {
+  const id = parseInt(Tools.getParam('id', options.url))
+  let tempData
+  extraData.extra.every(item => {
+    if (item.admin_id === id) {
+      tempData = {}
+      tempData.code = 0
+      tempData.msg = '操作成功'
+      tempData.extra = item
+      return false
+    }
+    return true
+  })
+  return tempData || dataEditError
+})
---
+++ src/views/admin/edit.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <el-dialog
+      :title="dialogFormTitle"
+      :fullscreen="fullscreen"
+      :visible.sync="dialogFormVisible"
+      :show-close="false"
+      :close-on-click-modal="true"
+      :close-on-press-escape="true"
+    >
+      <el-form :model="operForm" ref="operForm" :rules="rules" label-width="60px">
+        <el-form-item label="用户名" prop="admin_username">
+          <el-input v-model="operForm.admin_username" maxlength="20" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="admin_password">
+          <el-input
+            v-model="operForm.admin_password"
+            maxlength="20"
+            autocomplete="off"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="admin_password2">
+          <el-input maxlength="20" autocomplete="off" show-password></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="toggleDialog(false)">关 闭</el-button>
+        <el-button type="primary" @click="submitForm('operForm')">提交</el-button>
+        <el-button type="text" @click="toggleFullscreen()">
+          <i class="el-icon-full-screen dialogFullscreen"></i>
+        </el-button>
+        <el-button type="text" @click="toggleDialog(false)">
+          <i class="el-icon-circle-close dialogClose"></i>
+        </el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import http from '@/plugins/http'
+import '@/mock/Artsort'
+export default {
+  props: ['dialogFormTitle', 'dialogId'],
+  name: 'admin_edit',
+  data () {
+    return {
+      fullscreen: false,
+      dialogFormVisible: false,
+      dialogLastOperation: 'add',
+      operForm: {
+        admin_id: 0,
+        admin_username: '',
+        admin_password: ''
+      },
+      rules: {
+        admin_username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        admin_password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    submitForm: function (formName) {
+      const self = this
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          const url = self.$parent.dialogId === 0 ? '/Admin/add' : '/Admin/edit'
+          http.send({ sendType: 'post', url: url, param: this.operForm, showSuccessTip: true }).then(data => {
+            if (data.code === 0) {
+              self.$refs[formName].resetFields()
+              self.toggleDialog(false)
+              self.$parent.loadMine()
+            }
+          })
+        }
+      })
+    },
+    toggleDialog: function (flag) {
+      this.dialogFormVisible = flag
+    },
+    toggleFullscreen: function () {
+      this.fullscreen = !this.fullscreen
+    }
+  },
+  watch: {
+    '$parent.dialogEditTime': function () {
+      this.operForm.artsort_id = this.$parent.dialogId
+      if (this.$parent.dialogId === 0) {
+        if (this.dialogLastOperation === 'edit') {
+          this.$nextTick(() => {
+            this.$refs.operForm.resetFields()
+          })
+          this.dialogLastOperation = 'add'
+        }
+      } else {
+        this.dialogLastOperation = 'edit'
+        this.$nextTick(() => {
+          this.$refs.operForm.resetFields()
+        })
+        http.send({ url: '/Admin/detail', param: { params: { id: this.$parent.dialogId } } }).then(data => {
+          this.operForm = data.extra
+        })
+      }
+    }
+  }
+}
+</script>
