1cf91d056435675901c78ec5fb673052140592fc
Willson <tafengnanhai@foxmail.com>
2019-06-02 16:44:40 +0800

    消息测试

--- src/views/message/edit.vue
+++ src/views/message/edit.vue
@@ -2,15 +2,17 @@
   <div>
     <el-card class="statCard">
       <el-form :model="operForm" ref="operForm" :rules="rules" label-width="80px">
-        <el-form-item label="新注册" prop="reg_new">
+        <el-form-item label="是否提醒" prop="reg_new">
           <el-switch v-model="operForm.reg_new"></el-switch>
         </el-form-item>
-        <el-form-item label="注册数" prop="reg_total">
+        <el-form-item label="新增数" prop="reg_total">
           <el-input v-model.number="operForm.reg_total" maxlength="4" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="说明">消息提醒时用户级别的，不同的系统用户独立设置</el-form-item>
+        <el-form-item label="说明">不同的系统用户独立设置</el-form-item>
         <el-form-item>
           <el-button type="primary" @click="submitForm('operForm')">提交</el-button>
+          <el-button @click="demo()">测试消息</el-button>
+          <el-button @click="close()">全部关闭</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -20,6 +22,7 @@
 <script>
 import http from '@/plugins/http'
 import '@/mock/Message'
+import { Notification } from 'element-ui'
 export default {
   props: ['dialogFormTitle', 'dialogId'],
   name: 'admin_edit',
@@ -32,7 +35,7 @@ export default {
         reg_total: 1
       },
       rules: {
-        reg_total: [{ required: true, type: 'integer', min: 1, max: 9999, message: '请输入注册数1-9999', trigger: 'blur' }]
+        reg_total: [{ required: true, type: 'integer', min: 1, max: 9999, message: '请输入新增数（1-9999）', trigger: 'blur' }]
       }
     }
   },
@@ -48,6 +51,22 @@ export default {
           })
         }
       })
+    },
+    demo: function () {
+      Notification.success({
+        title: '消息提醒',
+        message: '相比上次增加了256名注册会员',
+        duration: 10000,
+        showClose: false,
+        position: 'bottom-right',
+        onClick: () => {
+          this.close()
+          // TODO:打开会员信息界面
+        }
+      })
+    },
+    close: function () {
+      Notification.closeAll()
     },
     loadMine: function () {
       http.send({ url: '/Message/detail' }).then((data) => {
