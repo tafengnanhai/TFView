3d059993adcbfafdc9c4b6bcd8f38f86e6251432
Willson <tafengnanhai@foxmail.com>
2019-06-03 11:07:47 +0800

    消息提醒（多用户控制）

--- src/mock/Message.js
+++ src/mock/Message.js
@@ -1,12 +1,21 @@
 import Mock from 'mockjs'
+import store from '@/store'
 
-const data = {
+const dataListAll = {
   code: 0,
   msg: '操作成功',
-  extra: {
-    reg_new: true,
-    reg_total: 1
-  }
+  extra: [
+    {
+      user_id: 1,
+      reg_new: false,
+      reg_total: 10
+    },
+    {
+      user_id: 2,
+      reg_new: true,
+      reg_total: 1
+    }
+  ]
 }
 
 const dataSuccess = {
@@ -16,8 +25,32 @@ const dataSuccess = {
 
 Mock.mock(/\/Message\/edit/, options => {
   const result = JSON.parse(options.body)
-  data.extra = result
+  dataListAll.extra = dataListAll.extra.map(item => {
+    if (item.user_id === store.state.userid) {
+      Object.assign(item, result)
+    }
+    return item
+  })
   return dataSuccess
 })
 
-Mock.mock(/\/Message\/detail/, 'get', data)
+let data = {
+  code: 0,
+  msg: '操作成功',
+  extra: {
+    user_id: 1,
+    reg_new: true,
+    reg_total: 1
+  }
+}
+
+Mock.mock(/\/Message\/detail/, 'get', function (options) {
+  dataListAll.extra.every(item => {
+    if (item.user_id === store.state.userid) {
+      data.extra = item
+      return false
+    }
+    return true
+  })
+  return data
+})
