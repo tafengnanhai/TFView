c3fe921d713e3b16b041035f05d82c459a762a4c
Willson <tafengnanhai@foxmail.com>
2019-06-06 10:10:25 +0800

    实时活跃增加全屏个功能

--- src/views/member/stat-now.vue
+++ src/views/member/stat-now.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="memberStatNow">
-    <el-card class="chartCard">
+    <el-card :class="chartCardClass" id="chartCard">
+      <div class="fullscreenButton">
+        <el-button type="text" @click="toggleFullscreen()">
+          <i class="el-icon-full-screen" style="font-size:40px"></i>
+        </el-button>
+      </div>
       <map-province
         :jsonData="jsonData"
         :cityData="cityData"
@@ -17,19 +22,31 @@ import MapProvince from '@/components/map-province.vue'
 import '@/mock/Member'
 import http from '@/plugins/http'
 export default {
-  name: 'member-stat-city',
+  name: 'member-stat-now',
   components: {
     MapProvince
   },
   data () {
     return {
+      chartCardClass: 'normal',
       jsonData: jsonData,
       cityData: [],
       timer: null,
-      interval: 1000
+      interval: 5000
     }
   },
   methods: {
+    toggleFullscreen: function () {
+      if (this.chartCardClass === 'normal') {
+        this.chartCardClass = 'fullscreen'
+        document.getElementById('chartCard').style.paddingTop = `${(document.body.clientHeight - document.getElementById('memberStatNow').clientHeight) / 2}px`
+        document.getElementById('chartCard').style.paddingLeft = `${(document.body.clientWidth - document.getElementById('memberStatNow').clientWidth) / 2}px`
+      } else {
+        document.getElementById('chartCard').style.paddingTop = 0
+        document.getElementById('chartCard').style.paddingLeft = 0
+        this.chartCardClass = 'normal'
+      }
+    },
     loadMine: function () {
       try { this.timer && clearInterval(this.timer) } catch (error) { }
       this.timer = setInterval(() => {
@@ -58,7 +75,21 @@ export default {
 #memberStatNow {
   min-width: 660px;
 }
-.chartCard {
-  margin-bottom: 20px;
+.fullscreenButton {
+  position: absolute;
+  right: 20px;
+  top: 10px;
+  z-index: 888;
+}
+.normal {
+  position: relative;
+}
+.fullscreen {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1000;
 }
 </style>
